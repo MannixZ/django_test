d9c398be845971401fd1d4c3b47c2f052d7db30f
MannixZ <317588213@qq.com>
2020-04-21 23:08:15 +0800

    Update 001-stages-declarative-style.groovy

--- jenkinsfiles/001-stages-declarative-style.groovy
+++ jenkinsfiles/001-stages-declarative-style.groovy
@@ -25,6 +25,15 @@ pipeline {
         }
       }
     }
+    
+    stage('Stage 4') {
+      when {
+        branch 'master'
+         steps {
+          echo 'i am master'
+        }
+      }
+    }
 
   }
 }
